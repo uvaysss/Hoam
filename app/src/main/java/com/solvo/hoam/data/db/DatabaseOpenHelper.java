b71b2cd6e352c184edd3f13f823ac22cb09b013a
uvaysss <uvais1994@gmail.com>
2017-07-21 19:34:43 +0300

    Added drop sql query

--- app/src/main/java/com/solvo/hoam/data/db/DatabaseOpenHelper.java
+++ app/src/main/java/com/solvo/hoam/data/db/DatabaseOpenHelper.java
@@ -3,6 +3,7 @@ package com.solvo.hoam.data.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.solvo.hoam.data.db.table.AdTable;
 import com.solvo.hoam.data.db.table.CategoryTable;
@@ -11,6 +12,7 @@ import com.solvo.hoam.data.db.table.LocationTable;
 
 public class DatabaseOpenHelper extends SQLiteOpenHelper {
 
+    private static final String TAG = DatabaseOpenHelper.class.getCanonicalName();
     private static final String DATABASE_NAME = "hoam";
     private static final int DATABASE_VERSION = 1;
 
@@ -24,10 +26,19 @@ public class DatabaseOpenHelper extends SQLiteOpenHelper {
         db.execSQL(LocationTable.SQL_CREATE_TABLE);
         db.execSQL(AdTable.SQL_CREATE_TABLE);
         db.execSQL(ImageTable.SQL_CREATE_TABLE);
+
+        Log.i(TAG, "SQLite tables created");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(CategoryTable.SQL_DROP_TABLE);
+        db.execSQL(LocationTable.SQL_DROP_TABLE);
+        db.execSQL(AdTable.SQL_DROP_TABLE);
+        db.execSQL(ImageTable.SQL_DROP_TABLE);
 
+        Log.i(TAG, "SQLite tables dropped");
+
+        onCreate(db);
     }
 }
